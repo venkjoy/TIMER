6118fb5a0bc1a8f27bdd55494cee9a3ada4ba2b8
venkjoy <143191328+venkjoy@users.noreply.github.com>
2023-08-29 19:18:54 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private int seconds=0;
+private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        starttimer();
+    }
+    public void onstart(View view){
+        running=true;
+
+    }
+    public void onstop(View view){
+        running=false;
+
+
+    }
+    public void onreset(View view){
+        running=false;
+        seconds=0;
+
+
+    }
+    private void starttimer(){
+        TextView timer=findViewById(R.id.timer);
+        Handler handler=new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                  int hrs=seconds/3600;
+                  int mins=(seconds%3600)/60;
+                  int sec=seconds%60;
+                  String time=String.format("%02d:%02d:%02d",hrs,mins,sec);
+                  timer.setText(time);
+                  if(running){
+                     seconds++;
+                  }
+                  handler.postDelayed(this,0);
+
+            }
+        });
+    }
+    }
